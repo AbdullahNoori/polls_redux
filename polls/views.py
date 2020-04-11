5c9af2ff742b28b9ffb53fa1367e94b019321f80
AbdullahNoori <nooriabdullah86@gmail.com>
2020-04-11 03:51:26 -0700

    completed django tutorial part 3

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,18 @@
 
 # Create your views here.
-# from django.http import HttpResponse
-from django.shortcuts import render
-
-from .models import Question
-
-from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
-from .models import Question
-# from django.http import Http404
-# from django.shortcuts import render
-from django.shortcuts import get_object_or_404, render
 
-from .models import Question
+
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Choice, Question
+
+
 # ...
+
 
 
 def index(request):
@@ -22,22 +20,9 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
-
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-    # return HttpResponse(template.render(context, request))
-# Leave the rest of the views (detail, results, vote) unchanged
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -46,16 +31,7 @@ def results(request, question_id):
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
-# def detail(request, question_id):
-#     try:
-#         question = Question.objects.get(pk=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question does not exist")
-#     return render(request, 'polls/detail.html', {'question': question})
 
 #  Django provides a shortcut. Here’s the detail() view, rewritten:
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
 
 # Leave the rest of the views (detail, results, vote) unchanged
